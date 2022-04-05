9b012e4c3bd8bcb3726db46bf96428c886c1b5c6
justsaykk <kaikein.woo@gmail.com>
2022-04-05 19:32:50 +0800

    committing initial Schema draft

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+import mongoose from "mongoose";
+const { schema } = mongoose;
+
+const userAccounts = new Schema({
+  userName: { String, required: true },
+  accountName: String,
+  accountDesc: String,
+  accountBalance: {
+    lastTwoMonth: Number,
+    lastMonth: Number,
+    thisMonth: Number,
+  },
+});
+
+const transactionCategories = new Schema({
+  tCode: String,
+  tCategory: String,
+});
+
+const userDetails = new Schema({
+  fullName: String,
+  email: String,
+  userName: String,
+  password: String,
+  accountName: [String],
+});
+
+const transactionDetails = new Schema({
+  userName: { String, required: true }, // Should be able to get this from the login
+  date: { type: Date, default: Date.now },
+  accountName: { String, required: true }, // Important to track money flow
+  amount: { Number, required: true }, // If user choose debit, postive. Else negative
+  senderName: String,
+  recipeintName: String,
+  userComments: [{ body: String, date: Date }],
+});

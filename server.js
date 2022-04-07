61197c0d8d7186fb4006a20048e38c252d92451c
justsaykk <kaikein.woo@gmail.com>
2022-04-07 20:09:22 +0800

    installed express session and touch .env

--- server.js
+++ server.js
@@ -1,38 +1,6 @@
-import mongoose from "mongoose";
-const { schema } = mongoose;
+// DEPENDENCIES
+const express = require("express");
 
-const userAccounts = new Schema({
-  userName: { String, required: true },
-  accountName: [{ String, required: true }],
-  accountDesc: String,
-  accountBalance: {
-    lastTwoMonth: Number,
-    lastMonth: Number,
-    thisMonth: Number,
-  },
-});
-
-const transactionCategories = new Schema({
-  tCode: String,
-  tCategory: String,
-});
-
-const userDetails = new Schema({
-  fullName: String,
-  email: String,
-  userName: String,
-  password: String,
-  accountName: [String],
-});
-
-const transactionDetails = new Schema({
-  userName: { String, required: true }, // Should be able to get this from the login
-  date: { type: Date, default: Date.now }, // To bucket the balance
-  accountName: { String, required: true }, // Important to track money flow
-  flow: { type: Boolean, required: true }, // true = debit, false = credit
-  amount: { Number, required: true }, // If user choose debit, postive. Else negative
-  tCategory: String, // Optional, only for data analysis and categorization
-  senderName: String, // Optional, only for data analysis and categorization
-  recipeintName: String, // Optional, only for data analysis and categorization
-  userComments: [{ body: String, date: Date }], // Optional, only for data analysis and categorization
-});
+// CONFIG
+const app = express();
+const PORT = process.env.PORT;

5b68ee6cd29c8cffec5fcfbf107d34cd390a055d
justsaykk <kaikein.woo@gmail.com>
2022-04-07 20:42:51 +0800

    required dotenv

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 // DEPENDENCIES
+require("dotenv").config();
 const express = require("express");
-// const session = require("express-session");
 
 // CONFIG
 const app = express();
-const PORT = 3000;
-
-// MIDDLEWARE
-// app.use(
-//   session({
-//     secret: process.env.SECRET, //a random string do not copy this value or your stuff will get hacked
-//     resave: false, // default more info: https://www.npmjs.com/package/express-session#resave
-//     saveUninitialized: false, // default  more info: https://www.npmjs.com/package/express-session#resave
-//   })
-// );
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello world");

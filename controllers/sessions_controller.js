e142a572fb1e88a6d0dc2d1c8f263a99f9703f29
justsaykk <kaikein.woo@gmail.com>
2022-04-09 16:50:49 +0800

    removed redirect to /dashboard

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -28,7 +28,7 @@ sessions.post("/", async (req, res) => {
       if (bcrypt.compareSync(req.body.password, foundUser.password)) {
         req.session.currentUser = foundUser;
         console.log(foundUser, " is logged in!");
-        res.redirect("/dashboard");
+        // res.redirect("/dashboard");
       } else {
         res.send('<a href="/">password does not match</a>');
       }

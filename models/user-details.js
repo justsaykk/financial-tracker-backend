e91f5bfbc776e999931e85ea1367b4103c1618b4
justsaykk <kaikein.woo@gmail.com>
2022-04-14 19:18:09 +0800

    added more information to the user-details schema

--- models/user-details.js
+++ models/user-details.js
@@ -6,6 +6,10 @@ const userDetailsSchema = new Schema({
   name: String,
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  accountName: { type: String, required: true },
+  accountType: { type: String, required: true },
+  accountDescription: String,
+  accountBalance: { type: Number, required: true },
 });
 
 const UserDetails = mongoose.model("UserDetails", userDetailsSchema);

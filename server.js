8ebff7e615671a3fc3c8b72c585f068c28a6c511
justsaykk <kaikein.woo@gmail.com>
2022-04-19 22:24:42 +0800

    changed cors whitelist

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(
   cors({
     credentials: true,
     origin: [
-      "https://localhost:3000",
-      "https://localhost:2000",
-      "https://localhost:4000",
+      "http://localhost:3000",
+      "http://localhost:2000",
+      "http://localhost:4000",
       "https://moneybank.vercel.app",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
